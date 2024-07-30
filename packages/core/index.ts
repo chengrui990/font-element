8c46697d3df865f3a1828bed287da78a62b2b277
crui990 <990536551@qq.com>
2024-07-30 23:16:11 +0800

    配置打包2

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -7,5 +7,5 @@ import '@font-element/theme/index.css'
 library.add(fas)
 const installer = makeInstaller(components)
 
-export * from '@font-element/components'
+export * from '../components'
 export default installer
